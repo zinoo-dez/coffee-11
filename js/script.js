7e7dfc666ff42965bae1f97dd2c117f0b993f8cd
zinoo-dez <zinoo.dev@gmail.com>
2025-07-09 18:19:34 +0630

    add carousel

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-const menu = document.querySelector(".menu-icon")
-const navItems = document.querySelector(".nav-items")
+const menu = document.querySelector(".menu-icon");
+const navItems = document.querySelector(".nav-items");
 // console.log('menu', menu)
 // console.log('navItems', navItems)
-menu.addEventListener('click',function(){
-    // alert(123)
-    navItems.classList.toggle('show')
-    if(navItems.classList.contains('show')){
-        menu.src = "./photos/svgs/close.svg"
-    }else{
-        menu.src = "./photos/svgs/menu.svg"
-    }
-})
-window.addEventListener("scroll",function(){
-    navItems.classList.remove("show")
-     menu.src = "./photos/svgs/menu.svg"
-})
+menu.addEventListener("click", function () {
+  // alert(123)
+  navItems.classList.toggle("show");
+  if (navItems.classList.contains("show")) {
+    menu.src = "./photos/svgs/close.svg";
+  } else {
+    menu.src = "./photos/svgs/menu.svg";
+  }
+});
+window.addEventListener("scroll", function () {
+  navItems.classList.remove("show");
+  menu.src = "./photos/svgs/menu.svg";
+});
